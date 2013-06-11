64ff46b2b9df2a5f764cea0f3f4724d3f5be9240
Devesh Chanchlani <vibhu@Vibhu-Srinivasans-MacBook-Pro-17.local>
2013-06-11 11:51:10 +0530

    US1 - As an international accountant I need a simple RPN adding machine so that I can easily manage my clients books.
    1: All values and operators are delimited by whitespace.
    2: Accepts a continuous stream of values and operators.
    3: Supports operations for sum, difference, division, multiplication.
    4: Supports floating point operations.
    5: Console input processed after every 'enter' key-press.
    6: When an operator is read
    - Output the list of all remaining values entered followed by the operator
    - Perform the operation
    - Display the resulting list of values awaiting further operators.

--- src/main/java/com/siq/RpnCalc.java
+++ src/main/java/com/siq/RpnCalc.java
@@ -1,25 +1,60 @@
 package com.siq;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class RpnCalc {
 
-	public int calculate(String inputString) throws Exception {
+	public Double calculate(String inputString) throws Exception {
 		StringTokenizer stringTokenizer = new StringTokenizer(inputString);
-		List<String> inputValues = new ArrayList<String>();
+		
+		Stack<String> rpnStack = new Stack<String>();
+		Stack<String> rpnOperatorStack = new Stack<String>();
+		Stack<Double> rpnOperandStack = new Stack<Double>();
+		
 		while(stringTokenizer.hasMoreElements()) {
-			inputValues.add((String) stringTokenizer.nextElement());
+			String element = (String) stringTokenizer.nextElement();
+			rpnStack.push(element);
 		}
 		
-		try {
-			Integer.parseInt(inputValues.get(0));
-		} catch (Exception e) {
-			throw new Exception("invalid input");
+		while(!rpnStack.isEmpty()) {
+			String value = rpnStack.pop();
+			try {
+				Double operand = Double.parseDouble(value);
+				rpnOperandStack.push(operand);
+			} catch (Exception e) {
+				rpnOperatorStack.push(value);
+			}
+		}
+
+		while(!rpnOperatorStack.isEmpty()) {
+			String operator = rpnOperatorStack.pop();
+			System.out.print(rpnOperandStack.toString());
+			Double operand1, operand2;
+			
+			try {
+				operand1 = rpnOperandStack.pop();
+				operand2 = rpnOperandStack.pop();
+			} catch (Exception e) {
+				throw new Exception("invalid input");
+			}
+			
+			Double result = 0d;
+			if(operator.compareTo("+") == 0) {
+				result = operand1 + operand2;
+			} else if(operator.compareTo("-") == 0) {
+				result = operand1 - operand2;
+			} else if(operator.compareTo("*") == 0) {
+				result = operand1 * operand2;
+			} else if(operator.compareTo("/") == 0) {
+				result = operand1 / operand2;
+			}
+			result = Math.round( result * 100.0 ) / 100.0;
+			
+			rpnOperandStack.push(result);
 		}
 		
-		return Integer.parseInt(inputValues.get(0)) + Integer.parseInt(inputValues.get(1));
+		return rpnOperandStack.pop();
 	}
 
 }
--- src/test/java/com/siq/RpnCalcTest.java
+++ src/test/java/com/siq/RpnCalcTest.java
@@ -1,19 +1,30 @@
-
 package com.siq;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+import java.io.ByteArrayOutputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
+
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 public class RpnCalcTest {
 	RpnCalc rpnCalc;
+	private OutputStream outContent = new ByteArrayOutputStream();
 	
 	@Before
 	public void setUp() {
 		rpnCalc = new RpnCalc();
+		System.setOut(new PrintStream(outContent));
+	}
+	
+	@After
+	public void after() {
+		System.setOut(null);
 	}
 	
 	@Test
@@ -40,15 +51,159 @@ public class RpnCalcTest {
 	}
 	
 	@Test
-	public void shouldAdd() {
+	public void testAdd() {
 		String inputString = "1 2 +";
-		int actualResult = 0;
+		Double actualResult = 0d;
 		try {
 			actualResult = rpnCalc.calculate(inputString);
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		assertEquals(3, actualResult);
+		assertEquals(new Double(3), actualResult);
+	}
+	
+	@Test
+	public void testAddFloatingNumbers() {
+		String inputString = "1.2 2.1 +";
+		Double actualResult = 0d;
+		try {
+			actualResult = rpnCalc.calculate(inputString);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		assertEquals(new Double(3.3d), actualResult);
+	}
+	
+	@Test
+	public void testSubtract() {
+		String inputString = "1 2 -";
+		Double actualResult = 0d;
+		try {
+			actualResult = rpnCalc.calculate(inputString);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		assertEquals(new Double(-1), actualResult);
+	}
+	
+	@Test
+	public void testSubFloatingNumbers() {
+		String inputString = "1.2 0.1 -";
+		Double actualResult = 0d;
+		try {
+			actualResult = rpnCalc.calculate(inputString);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		assertEquals(new Double(1.1d), actualResult);
+	}
+	
+	@Test
+	public void testMultiply() {
+		String inputString = "3 2 *";
+		Double actualResult = 0d;
+		try {
+			actualResult = rpnCalc.calculate(inputString);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		assertEquals(new Double(6), actualResult);
+	}
+	
+	@Test
+	public void testMultiplyFloatingNumbers() {
+		String inputString = "1.2 0.1 *";
+		Double actualResult = 0d;
+		try {
+			actualResult = rpnCalc.calculate(inputString);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		assertEquals(new Double(0.12d), actualResult);
+	}
+	
+	@Test
+	public void testDivide() {
+		String inputString = "3 2 /";
+		Double actualResult = 0d;
+		try {
+			actualResult = rpnCalc.calculate(inputString);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		assertEquals(new Double(1.5), actualResult);
+	}
+	
+	@Test
+	public void testDivideFloatingNumbers() {
+		String inputString = "1.2 0.1 /";
+		Double actualResult = 0d;
+		try {
+			actualResult = rpnCalc.calculate(inputString);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		assertEquals(new Double(12d), actualResult);
+	}
+	
+	@Test
+	public void testAddTwice() {
+		String inputString = "1 2 + 3 +";
+		Double actualResult = 0d;
+		try {
+			actualResult = rpnCalc.calculate(inputString);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		assertEquals(new Double(6), actualResult);
+	}
+	
+	@Test
+	public void testSubTwice() {
+		String inputString = "1 2 - 3 -";
+		Double actualResult = 0d;
+		try {
+			actualResult = rpnCalc.calculate(inputString);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		assertEquals(new Double(-4), actualResult);
+	}
+	
+	@Test
+	public void testDivideTwice() {
+		String inputString = "12 2 / 3 /";
+		Double actualResult = 0d;
+		try {
+			actualResult = rpnCalc.calculate(inputString);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		assertEquals(new Double(2), actualResult);
+	}
+	
+	@Test
+	public void testMultiplyTwice() {
+		String inputString = "1 2 * 3 *";
+		Double actualResult = 0d;
+		try {
+			actualResult = rpnCalc.calculate(inputString);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		assertEquals(new Double(6), actualResult);
+	}
+	
+	@Test
+	public void testDisplayValuesFollowingOperator() {
+
+		String inputString = "1 2 * 3 + 4 - 5 /";
+		try {
+			rpnCalc.calculate(inputString);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		assertEquals("[5.0, 4.0, 3.0, 2.0, 1.0][5.0, 4.0, 3.0, 2.0][5.0, 4.0, 5.0][5.0, 1.0]", 
+				outContent.toString());
 	}
 }
